daf0d7219cfef7016c59a07dd2bb5809128862c4
arthurbirate <biratearthur@gmail.com>
2023-09-28 22:24:03 +0200

    ok2

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ container = st.container()
 container.write(" cryptarithmetic puzzle is a mathematical exercise where the digits of some numbers are represented by letters (or symbols).Each letter represents a unique digit. The goal is to find the digits such that a given mathematical equation is verified:")
 
 container.write("For example: ")
-user_input = st.text_input("Enter Your Puzzle ")
+user_input = st.text_input("Enter Your Puzzles ")
 def extract(input_user):
     letters = []
     set_letters = set(letters)
