d84489a0fee971e9a4885655ef07ca6f68a849ba
arthurbirate <biratearthur@gmail.com>
2023-09-29 01:06:00 +0200

    tr

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,21 @@
-
-import streamlit as st 
-
+from simpleai.search import CspProblem, backtrack
+import streamlit as st
 import re
+
+# Set Streamlit page config
 st.set_page_config(layout="centered")
-
-
-st.title(" Cryptarithmetic puzzles 🧩 ")
+st.title("Cryptarithmetic Puzzles 🧩")
 st.divider()
 container = st.container()
 
-
-container.write(" cryptarithmetic puzzle is a mathematical exercise where the digits of some numbers are represented by letters (or symbols).Each letter represents a unique digit. The goal is to find the digits such that a given mathematical equation is verified:")
-
-container.write("For example: ")
-user_input = st.text_input("Enter Your Puzzles ")
+# Explanation and user input
+container.write("A cryptarithmetic puzzle is a mathematical exercise where the digits of some numbers are represented by letters (or symbols). Each letter represents a unique digit. The goal is to find the digits such that a given mathematical equation is verified.")
+container.write("For example:")
+user_input = st.text_input("Enter Your Puzzle")
 
 new_input = re.split(r'\s+', user_input)
 
+# Extract unique letters from the input
 def extract(input_user):
     letters = set()
     for word in new_input:
@@ -27,39 +26,22 @@ def extract(input_user):
 
 user_variables = extract(user_input)
 
-domains = {}
+# Define domains for variables (initially 0-9 for all)
+domains = {variable: list(range(0, 10)) for variable in user_variables}
 
-for variable in user_variables:
-    domains[variable] = list(range(0, 10))
-
-# Update domains for variables found at the beginning of words to [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# Update domains for variables found at the beginning of words to [1-9]
 for variable in user_variables:
     for word in new_input:
         if variable == word[0]:
             domains[variable] = list(range(1, 10))
-          
 
-# st.write(domains) 
-    
-    # for variable in user_variables:
-    #     if word.startswith(variable):
-    #         domains[variable] = list(range(1, 10))
-    #     if not word.startswith(variable):
-    #         domains[variable] = list(range(0, 10))
-       
-    
-    
-
-
-# st.write(new_input)
+# Define constraints
 def constraint_unique(variables, values):
-    return len(values) == len(set(values))  # remove repeated values and count
+    return len(values) == len(set(values))  # Check for unique values
 
 def constraint_add(variables, values):
-    # Create a dictionary to map variables to their values
     variable_to_value = {var: val for var, val in zip(variables, values)}
 
-    # Calculate the sum based on new_input
     total = 0
     current_number = ''
     for item in new_input:
@@ -68,37 +50,17 @@ def constraint_add(variables, values):
         else:
             total += int(current_number)
             current_number = ''
-    total += int(current_number)  # Add the last number if any
+    total += int(current_number)
     return total
 
-
-
 constraints = [
-    (user_input, constraint_unique),
-    (user_input, constraint_add),
+    (user_variables, constraint_unique),
+    (user_variables, constraint_add),
 ]
-from simpleai.search import CspProblem, backtrack
 
-problem = CspProblem(user_input, domains, constraints)
+problem = CspProblem(user_variables, domains, constraints)
 
+# Solve the CSP problem
 output = backtrack(problem)
 
 st.write(output)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# st.text(" ")
-
