bbfeab6dcd7419c30b96c2fe8def15c8613855bc
arthurbirate <biratearthur@gmail.com>
2023-09-29 00:41:42 +0200

    jj

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,20 @@
-import streamlit as st 
+import streamlit as st
 import re
+from constraint import *
+
 st.set_page_config(layout="centered")
 
-
-st.title(" Cryptarithmetic puzzles 🧩 ")
+st.title("Cryptarithmetic puzzles 🧩")
 st.divider()
 container = st.container()
 
+container.write("Cryptarithmetic puzzle is a mathematical exercise where the digits of some numbers are represented by letters (or symbols). Each letter represents a unique digit. The goal is to find the digits such that a given mathematical equation is verified.")
 
-container.write(" cryptarithmetic puzzle is a mathematical exercise where the digits of some numbers are represented by letters (or symbols).Each letter represents a unique digit. The goal is to find the digits such that a given mathematical equation is verified:")
-
-container.write("For example: ")
+container.write("For example:")
 user_input = st.text_input("Enter Your Puzzles ")
 
 new_input = re.split(r'\s+', user_input)
+
 
 def extract(input_user):
     letters = set()
@@ -22,6 +23,7 @@ def extract(input_user):
             if char.isalpha():
                 letters.add(char)
     return letters
+
 
 user_variables = extract(user_input)
 
@@ -35,53 +37,24 @@ for variable in user_variables:
     for word in new_input:
         if variable == word[0]:
             domains[variable] = list(range(1, 10))
-          
 
-st.write(domains) 
-    
-    # for variable in user_variables:
-    #     if word.startswith(variable):
-    #         domains[variable] = list(range(1, 10))
-    #     if not word.startswith(variable):
-    #         domains[variable] = list(range(0, 10))
-       
-    
-    
+# Define constraints
+problem = Problem()
 
+# Add variables and domains
+for variable, domain in domains.items():
+    problem.addVariable(variable, domain)
 
-# st.write(new_input)
-# def constraint_unique(variables, values):
-#     return len(values) == len(set(values))  # remove repeated values and count
+# Define the equation as a constraint
+equation = "".join(new_input)
+problem.addConstraint(lambda *args: eval(equation, {var: val for var, val in zip(user_variables, args)}), user_variables)
 
-# def constraint_add(variables, values):
-#     factor = int(str(values[1]) + str(values[3]) + str(values[5]))
-#     result = int(str(values[0]) + str(values[5]) + str(values[2]) + str(values[4]))
-#     return (factor + factor) == result
+# Solve the problem using backtracking
+solutions = problem.getSolutions()
 
-# constraints = [
-#     (('F', 'T', 'U', 'W', 'R', 'O'), constraint_unique),
-#     (('F', 'T', 'U', 'W', 'R', 'O'), constraint_add),
-# ]
-
-# problem = CspProblem(variables, domains, constraints)
-
-# output = backtrack(problem)
-# print('\nSolutions:', output)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# st.text(" ")
-
+if solutions:
+    st.write("Solutions:")
+    for solution in solutions:
+        st.write({var: val for var, val in solution.items()})
+else:
+    st.write("No solutions found.")
