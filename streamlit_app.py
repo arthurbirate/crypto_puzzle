86925e7cf1f267530522ac99eae45dc004840014
arthurbirate <biratearthur@gmail.com>
2023-09-28 21:11:24 +0200

    push_1

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ import streamlit as st
 
 st.title(" Cryptarithmetic puzzles 🧩 ")
 
-st.text(" cryptarithmetic puzzle is a mathematical exercise where the digits of some numbers are represented by letters (or symbols). Each letter represents a unique digit. The goal is to find the digits such that a given mathematical equation is verified:")
+st.text(" cryptarithmetic puzzle is a mathematical exercise where the digits of some numbers are represented by letters (or symbols).")
+st.text(" Each letter represents a unique digit. The goal is to find the digits such that a given mathematical equation is verified:")
 
 # variable_1 = st.text_input("Enter variable")
 
